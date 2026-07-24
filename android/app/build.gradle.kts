2497a287b82d989fabd3e1b89122e4d151dd69b2
danijr23 <riverajafeth.11@gmail.com>
2026-07-23 21:47:05 -0600

    chore: reorganize auth screens and update android firebase wiring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
     // END: FlutterFire Configuration
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.mi_inventario"
+    namespace = "com.example.listafirebase"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -19,7 +20,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mi_inventario"
+        applicationId = "com.example.listafirebase"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
