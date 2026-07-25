7be914771339b89c9ffc001c64cceadc243a72e2
Gissell15 <gissellsamayoa2020@gmail.com>
2026-07-25 00:17:39 -0600

    Merge branch 'main' into Gissell

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:mi_inventario/auth/services/auth_service.dart';
 import 'package:mi_inventario/controller/productos_controller.dart';
 import 'package:mi_inventario/login/login_screen.dart';
-import 'package:mi_inventario/categoria/categoria_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
---
+++ lib/model/dashboard_model.dart
@@ -0,0 +1,10 @@
+
+class DashboardModel {
+  final String titulo;
+  final IconData icono;
+
+  DashboardModel({
+    required this.titulo,
+    required this.icono,
+  });
+}
