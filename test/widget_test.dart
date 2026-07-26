eba9fc7429b22f0231991bdbae1b1eae54ce2723
danijr23 <riverajafeth.11@gmail.com>
2026-07-25 22:55:22 -0600

    test: fix login startup widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,9 @@
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mi_inventario/firebase_options.dart';
 import 'package:mi_inventario/main.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  setupFirebaseCoreMocks();
-
   testWidgets('shows login screen on startup', (WidgetTester tester) async {
-    await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
     await tester.pumpWidget(const MyApp());
     await tester.pump(const Duration(seconds: 1));
 
